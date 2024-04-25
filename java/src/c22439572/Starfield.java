3f89476982512c3e5d2aa23ef8beca088e096bd9
MeechBMF <C22439572@mytudublin.ie>
2024-04-25 13:51:19 +0100

    second image made

--- java/src/c22439572/Starfield.java
+++ java/src/c22439572/Starfield.java
@@ -2,7 +2,8 @@ package c22439572;
 
 public class Starfield extends MichaelsVisuals{
 
-    Star[] stars = new Star[100];
+    static final int NUM_LINES = 10;
+    float t;
 
     public void settings()
     {
@@ -11,21 +12,37 @@ public class Starfield extends MichaelsVisuals{
 
     public void setup()
     {
-        for (int i = 0; i < stars.length; i++)
-        {
-            stars[i] = new Star();
-        }
+        background(20);
     }
 
     public void draw()
     {
-        background(0);
+        background(20);
+        stroke(255);
+        strokeWeight(5);
+
         translate(width/2, height/2);
-        for (int i = 0; i < stars.length; i++)
+        for (int i = 0; i < NUM_LINES; i++)
         {
-            stars[i].update();
-            stars[i].show();
+            line(x1(t + i), y1(t + i), x2(t + i), y2(t + i));
         }
+        t++;
+    }
+
+    float x1(float t){
+        return sin(t / 10) * 100 + sin(t / 5) * 20;
+    }
+
+    float y1(float t){
+        return cos(t / 10) * 100;
+    }
+
+    float x2(float t){
+        return sin(t / 10) * 200 + sin(t) * 2;
+    }
+
+    float y2(float t){
+        return cos(t / 20) * 200 + cos(t / 12) * 20;
     }
     
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new Terrain());
+        processing.core.PApplet.runSketch(a, new Starfield());
     }
 
     public static void main(String[] args) {
